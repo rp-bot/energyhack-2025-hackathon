acb60177afc76920e8fa250297ed9e3086570e95
rpbot_mac <71392285+rp-bot@users.noreply.github.com>
2025-01-18 19:46:39 -0500

    updated some shit

---
+++ frontend-energyhack-2025/src/app/components/nav-bar.component.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import logo from "/public/logo.png";
+
+export function NavBar ()
+{
+	return (
+		<>
+			<header className="w-full p-5 flex flex-row justify-center items-center gap-2">
+				<div className="bg-white rounded-full">
+					<Image src={ logo } alt={ "logo png" } width={ 300 } height={ 300 } className="w-20 h-20 object-cover" />
+				</div>
+				<h1 className="font-mono text-2xl font-bold text-zinc-950">
+					r3Charge
+				</h1>
+			</header>
+		</>
+	);
+}
